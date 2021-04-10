360c96998173cde4f750ef63d8f9d581de0c4f01
abdulvahap önen <82175373+abdulvahaponen@users.noreply.github.com>
2021-04-10 21:36:27 +0300

    Update StringTutorial.java

--- StringTutorial/src/StringTutorial.java
+++ StringTutorial/src/StringTutorial.java
@@ -11,26 +11,26 @@ String subStringDeger=stringDeger.substring(8);
 System.out.println(subStringDeger);
 String subbStinrgeger2=stringDeger.substring(8,18);
 System.out.println(subbStinrgeger2);
-String stringDeger3="merhaba mobilhanem.com";
+String stringDeger3="hello world";
 System.out.println(stringDeger3.toUpperCase());
 System.out.println(stringDeger3.toLowerCase());
 System.out.println(stringDeger.replace("r","s"));
 System.out.println(stringDeger.replace("merhaba", "hello"));
-boolean durum=stringDeger.equals("Merhaba mobilhanem.com");
+boolean durum=stringDeger.equals("hello world");
 System.out.println(durum);
-boolean durum2=stringDeger.equals("merhaba mobilhanem.com");
-boolean durum3=stringDeger.equalsIgnoreCase("merhaba mobilhanem.com");
+boolean durum2=stringDeger.equals("hello world");
+boolean durum3=stringDeger.equalsIgnoreCase("hello world");
 System.out.println(durum3);
 String[] stringDegerler=stringDeger.split(" ");
 System.out.println(stringDegerler[0]);
 System.out.println(stringDegerler[1]);
 String aString="merhaba";
-String bString="mobilhanem.com";
+String bString="hello world";
 System.out.println(aString+bString);
 String cString=aString+bString;
 System.out.println(charDeger);
 //veya
-System.out.println("merhaba"+"mobilhanem.com");
+System.out.println("merhaba"+"hello world");
 	
 	}
 }
